3c17f45f25d09f78f6ce4d14ca97e8290e45dc3d
jaroslavdextems <hel00win@yandex.ru>
2018-09-10 09:18:14 +0400

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const oracleDB = require('./server-oracle') // get oracledb utils
+
+const app = express()
+
+let state = { 
+    health: false,
+    message: "started",
+    application: "posdss_metrics_exporter"
+}
+
+function stateUpdate( healthValue ) {
+    state.health = healthValue
+}
+
+oracleDB.select(`select 5 from dual`)
+    .then( result => {
+        console.log ("OK:")
+        console.log( result )
+        stateUpdate( true ) 
+    })
+    .catch( err => {
+        console.log("ERROR:")
+        console.log( err )
+        stateUpdate( false ) 
+    })
+
+app.get('/health', function (req, res) {
+    res.jsonp(JSON.stringify( state ))
+})
+
+app.listen(3000)
