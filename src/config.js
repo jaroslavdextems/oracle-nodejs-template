0a691c61502ba56203fce900d47f67acbcba7214
jaroslavdextems <hel00win@yandex.ru>
2018-09-10 09:39:26 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,5 +5,8 @@ module.exports = {
     password: process.env.NODE_ORACLEDB_PASSWORD || "pass",
     connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/oracle-db-instance",
     externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false,
+    elasticURL: ['http://elastic-instance-1:9200/', 'http://elastic-instance-2:9200/'],
+    elasticTimeout: 180000,
+    esPackageSize: 10000, //Set it to 0 to don't use partly sending data to ElasticSearch
     oracleDBTimeout: 180
 };
